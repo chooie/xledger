b95a2eafa9a6ad4abba00e7b2437d0822ea8c752
Charlie Hebert <charlie.hebert92@gmail.com>
2016-08-01 08:26:39 -0600

    Make the beginning match code more clear

--- src/client/search.js
+++ src/client/search.js
@@ -16,20 +16,32 @@
     // Sample implemention, only respecting rule 2 : Matches at the beginning  
     var i,
         entry,
-        searchDataLength = SearchData.length, 
-        ret = [];
+        searchDataLength = SearchData.length,
+        searchMatches = [];
     
     for (i = 0; i < searchDataLength; i += 1) {
       entry = SearchData[i];
-      if (entry.substr(0, searchTerm.length).toLowerCase() === searchTerm.toLowerCase()) {
-        ret.push({value: entry, matches: [{start: 0, end: searchTerm.length}]});
+      if (isBeginningMatch(searchTerm, entry)) {
+        searchMatches.push({value: entry,
+                            matches: [{start: 0, end: searchTerm.length}]});
       }
     }
-    return ret.slice(0, 20);
+
+    var mostRelevantSubset = searchMatches.slice(0, 20);
+
+    return mostRelevantSubset;
   };
 
   function searchIsEmpty(searchTerm) {
     return !searchTerm || searchTerm.length === 0;
+  }
+
+  function isBeginningMatch(searchTerm, searchDataEntry) {
+    var lowerCaseSearchTerm = searchTerm.toLowerCase(),
+        lowerCaseSearchDataEntry = searchDataEntry.toLowerCase(),
+        entryUpToSearchLength =
+          lowerCaseSearchDataEntry.substr(0, searchTerm.length);
+    return lowerCaseSearchTerm === entryUpToSearchLength;
   }
 
   exports.run = function() {
